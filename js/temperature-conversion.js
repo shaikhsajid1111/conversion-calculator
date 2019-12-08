0fb330cbc7f603b2d361094f8fa2f35842639272
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2019-12-08 12:45:36 +0530

    added for temperature conversion

---
+++ js/temperature-conversion.js
@@ -0,0 +1,22 @@
+fahreinheitElement = document.getElementById("fahrenheit");
+celsiusElement = document.getElementById("celsius");
+kelvinElement = document.getElementById("kelvin");
+
+
+function fromFahrenheitToOther(f){
+    f = parseFloat(f);
+    celsiusElement.value = (f-32)/1.8;
+    kelvinElement.value = ((f-32)/1.8)+273.15;
+}
+
+function fromCelsiusToOthers(c){
+    c = parseFloat(c);
+    fahreinheitElement.value = (c*1.8)+32;
+    kelvinElement.value =c+273.15;
+}
+function fromKelvinToOther(k){
+    k = parseFloat(k);
+    fahreinheitElement.value = ((k-273.15)*1.8)+32;
+    celsiusElement.value = k-273.15;
+
+}
