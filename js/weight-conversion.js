992f9ad375732ef977aab26933e164051be23e29
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2019-12-08 11:26:26 +0530

    design modified

--- js/weight-conversion.js
+++ js/weight-conversion.js
@@ -6,6 +6,7 @@ ounce_element = document.getElementById("ounce");
 stone_element = document.getElementById("stone");
 carat_element = document.getElementById("carat");
 miligram_element = document.getElementById("miligram");
+metricTonElement = document.getElementById("metric-ton");
 /*from pounds to other conversions*/
 function fromPoundsToOther(pounds){
 
@@ -15,6 +16,7 @@ function fromPoundsToOther(pounds){
   stone_element.value = pounds*0.071429;     //to stone
   carat_element.value = pounds*2267.96;
   miligram_element.value = pounds*453592;
+  metricTonElement.value = pounds*0.000453592;
   
 }
 
@@ -26,6 +28,7 @@ function fromGramToOthers(gram){
   stone_element.value = gram*0.00015747;     //to stone
   carat_element.value = gram*5;
   miligram_element.value = gram*1;
+  metricTonElement.value = gram*0.000001;
 
 }
 
@@ -38,6 +41,7 @@ function fromKilogramToOther(kg){
   stone_element.value = kg*0.1574;    //to stone
   carat_element.value =  kg*0.0283495;
   miligram_element.value = kg*1000000;
+  metricTonElement.value = kg*0.001;
 }
 /*from Ounces To others*/
 function fromOuncesToOther(oz){
@@ -48,6 +52,7 @@ function fromOuncesToOther(oz){
   stone_element.value = oz*0.0044643;       //to stone
   carat_element.value = oz*141.7475;          //to carat
   miligram_element.value = oz*28349.5;          //to miligram
+  metricTonElement.value = oz*35.273990723;
 }
 
 /*from stone to others*/
@@ -59,6 +64,7 @@ function fromStoneToOthers(st){
   ounce_element.value = st*224;           //ounce
   carat_element.value = st*31751.47;        //to carat
   miligram_element.value = st/0.00000015747;      //to miligram
+  metricTonElement.value = st/157.4730;
 }
 
 /*From carat to others */
@@ -69,6 +75,7 @@ function fromCaratToOthers(ct){
     ounce_element.value = ct*0.0070547981;  //to ounce
     stone_element.value = ct / 31751.47;        //to stone
     miligram_element.value = ct*200;      //to mg
+    metricTonElement.value = ct*2.E-7;
   }
 
 /*from miligram to other conversions*/
@@ -79,5 +86,16 @@ function fromMiligramToOthers(mg){
   ounce_element.value = mg*0.000035274;     //to ounce
   stone_element.value = mg * 0.00000015747;     //to stone
   carat_element.value = mg*0.005;           //to carat
+  metricTonElement.value = mg*9.999999999E-10;
+}
+
+function fromMetricTonToOthers(mt){
+  pounds_element.value = mt*2204.6244202;     //to pounds
+  gram_element.value = mt*1000000;          //to grams
+  kilogram_element.value = mt*1000;       //to kg
+  ounce_element.value = mt*35273.990723;     //to ounce
+  stone_element.value = mt*157.473;     //to stone
+  carat_element.value = mt*5000000;           //to carat
+  miligram_element.value = mt*1000000000;
 }
 
